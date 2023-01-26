977e7519423d18bf7ececbf121e9e5b9e9eaf884
DragoDG <122324484+DragoDG@users.noreply.github.com>
2023-01-25 21:17:30 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var barco,barcoimg;
+var mar,marimg;
+function preload(){
+ barcoimg=loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+ marimg=loadImage("sea.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+  mar=createSprite(200,200,400,200)
+  mar.addImage("mar",marimg)
+  mar.scale=0.3
+
+
+  barco=createSprite(120,190,30,35)
+  barco.addAnimation("navegando",barcoimg)
+  barco.scale=0.3
+}
+
+function draw() {
+  background("blue");
+    drawSprites();
+  
+  
+  
+
+}
